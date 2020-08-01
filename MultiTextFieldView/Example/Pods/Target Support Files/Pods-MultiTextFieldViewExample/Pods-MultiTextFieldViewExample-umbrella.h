9f184ae50d158d161fb03b27e4abfac57c8950a8
Furkan KAPLAN <furkankaplan@outlook.com>
2020-08-01 14:30:34 +0300

    Example - Update for new repo name

--- MultiTextFieldView/Example/Pods/Target Support Files/Pods-MultiTextFieldViewExample/Pods-MultiTextFieldViewExample-umbrella.h
+++ MultiTextFieldView/Example/Pods/Target Support Files/Pods-MultiTextFieldViewExample/Pods-MultiTextFieldViewExample-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_OTPPinEntryViewExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_OTPPinEntryViewExampleVersionString[];
+FOUNDATION_EXPORT double Pods_MultiTextFieldViewExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MultiTextFieldViewExampleVersionString[];
 
